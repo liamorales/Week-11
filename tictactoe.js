1bf23becda31765b4f0e878ffb70919e3adc0e8a
liamorales <morales.lia.1990@gmail.com>
2022-08-12 19:26:31 -0500

    updating project

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,9 @@
+//setting up the game 
 function game(){
 
     document.getElementById("btn1").innerHTML="Start Game"
-
+/* activating the start button on the game and then defining each square in the tic 
+tac toe as variables, so that they can be clicked and chosesn a posssible option*/ 
 
     var id1 = document.querySelector("#td1")
     var id2 = document.querySelector("#td2")
@@ -13,6 +15,8 @@ function game(){
     var id8 = document.querySelector("#td8")
     var id9 = document.querySelector("#td9")
 
+/* Here is my function of how my board will work by a single click and a double 
+click*/
 
     id1.addEventListener("click", function(){
         id1.textContent = 'X'
@@ -51,7 +55,6 @@ function game(){
         id5.textContent = 'O'
     })
 
-
     id6.addEventListener("click", function(){
         id6.textContent = 'X'
     })
@@ -59,14 +62,12 @@ function game(){
         id6.textContent = 'O'
     })
 
-
     id7.addEventListener("click", function(){
         id7.textContent = 'X'
     })
     id7.addEventListener("dblclick",function(){
         id7.textContent = 'O'
     })
-
 
     id8.addEventListener("click", function(){
         id8.textContent = 'X'
@@ -81,4 +82,27 @@ function game(){
     id9.addEventListener("dblclick",function(){
         id9.textContent = 'O'
     })
+}   
+// function to declare the winner of game
+    
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
 }
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('You Won!', 'success')
+  })
+}
+
